ba69b0cc61a4f71ce9448d7bf4313bfa87476ef7
SarahAbdulaziz77 <sarahabdulaziiz76@gmail.com>
2024-09-08 02:50:41 +0300

    Update authentication_state.dart

--- blade/lib/blocs/authentication_bloc/authentication_state.dart
+++ blade/lib/blocs/authentication_bloc/authentication_state.dart
@@ -1,10 +1,25 @@
 part of 'authentication_bloc.dart';
 
-sealed class AuthenticationState extends Equatable {
-  const AuthenticationState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum AuthenticationStatus { authenticated, unauthenticated, unknown } //states the user can be in 
 
-final class AuthenticationInitial extends AuthenticationState {}
+class AuthenticationState extends Equatable {  //Equatable is a package that comes with flutter bloc which allow us to compare objects
+  const AuthenticationState._({
+    this.status = AuthenticationStatus.unknown,
+    this.user
+
+  });
+
+  final AuthenticationStatus status;
+  final MyUser? user;
+
+  const AuthenticationState.unknown() : this._(); //return the constructor itself and do nothing
+  const AuthenticationState.authenticated(MyUser myUser) :
+  this._(status: AuthenticationStatus.authenticated, user: myUser);
+
+  const AuthenticationState.unauthenticated() :
+  this._(status: AuthenticationStatus.unauthenticated); //not gonna add a user since we dont have one
+
+
+  @override
+  List<Object?> get props => [status, user];
+}
