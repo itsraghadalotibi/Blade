43514fdd8de1db6f3b312122d4dae75af8649dc9
SarahAbdulaziz77 <sarahabdulaziiz76@gmail.com>
2024-09-08 02:37:11 +0300

    edited (app, main.dart)

--- blade/lib/main.dart
+++ blade/lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:blade_app/app.dart';
+import 'package:blade_app/simple_bloc_observe.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+// where the app runs itself
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //to make sure that everything initilized right
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo())); //it is implement the abstract class
 }
 
 
-// where the app runs itself
+
+
