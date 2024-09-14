bf0c08f2073f132d9ecc0be0bc3640fd509367bc
yaral1l12 <161306090+yaral1l12@users.noreply.github.com>
2024-09-14 21:46:02 +0300

    fierbase for win set up
    fierbase for win set up

--- blade/lib/main.dart
+++ blade/lib/main.dart
@@ -9,7 +9,14 @@ import 'package:user_repository/user_repository.dart';
 // where the app runs itself
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //to make sure that everything initilized right
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: "AIzaSyCyJjv7oScBSeafBZGk6mSVv3U6sPYYmnU", 
+    appId: "1:284932571171:android:0597dda5ad92a34e883196", 
+    messagingSenderId: "284932571171", 
+    projectId: "blade-87cf7",
+    )
+    
+  );
   Bloc.observer = SimpleBlocObserver();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp(FirebaseUserRepo())); //it is implement the abstract class
