8e01dd80c855b92dbfaf5fcd1fb69b8197b99c16
yaral1l12 <161306090+yaral1l12@users.noreply.github.com>
2024-09-23 00:01:02 +0300

    u

--- blade/lib/features/authentication/bloc/authentication_state.dart
+++ blade/lib/features/authentication/bloc/authentication_state.dart
@@ -27,5 +27,23 @@ class AuthenticationFailure extends AuthenticationState {
 
   @override
   List<Object?> get props => [error];
+}class SkillsLoading extends AuthenticationState {}
+
+class SkillsLoaded extends AuthenticationState {
+  final List<String> availableSkills;
+
+  SkillsLoaded({required this.availableSkills});
+
+  @override
+  List<Object?> get props => [availableSkills];
+}
+
+class SkillsError extends AuthenticationState {
+  final String error;
+
+  SkillsError({required this.error});
+
+  @override
+  List<Object?> get props => [error];
 }
 
