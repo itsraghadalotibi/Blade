bd1f34b108c26fa8e82b232cca68ee8b0e93a36d
NajlaaHZ <147070686+NajlaaHZ@users.noreply.github.com>
2024-10-16 18:28:09 +0300

    return

--- blade/lib/features/announcement/src/announcement_repository.dart
+++ blade/lib/features/announcement/src/announcement_repository.dart
@@ -1,58 +1,58 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'announcement_model.dart';
 
-//yara
 class AnnouncementRepository {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
 
   AnnouncementRepository();
 
   // Method to create a new Idea in Firestore with the creator as the first member
   Future<void> createIdea(Idea idea, String creatorId) async {
     try {
+      // Ensure the creator is the first member of the idea
       if (idea.members.isEmpty || idea.members[0] != creatorId) {
         idea.members.insert(0, creatorId);
       }
-
+      
       await firestore.collection('ideas').add(idea.toMap());
     } catch (e) {
       throw Exception('Failed to create idea: $e');
     }
   }
 
+  
   // Fetch ideas where status='open' and not owned or already a member by the current user
   Future<List<Idea>> fetchIdeas(String currentUserId) async {
     try {
-      // Fetch open ideas
       final snapshot = await firestore
           .collection('ideas')
           .where('status', isEqualTo: 'open') // Filter for status='open'
           .get();
 
-      // Fetch join requests for the current user
-      final querySnapshot = await firestore
+      final querySnapshot = await FirebaseFirestore.instance
           .collection('join_requests')
           .where('userId', isEqualTo: currentUserId)
           .get();
 
-      // Map over the ideas and filter them
-      return snapshot.docs.where((doc) {
-        final members = List<String>.from(doc['members'] ?? []);
-        return members.isEmpty ||
-            (members[0] != currentUserId && !members.contains(currentUserId)) &&
-                members.length < doc["maxMembers"];
-      }).map((doc) {
-        final data = doc.data() as Map<String, dynamic>;
-
-        // Check if the user has a join request for this idea
-        final isPendingOrAccepted = querySnapshot.docs.any((d) {
-          return d['ideaId'] == doc.id &&
-              (d['status'] == 'pending' || d['status'] == 'accepted');
-        });
-
-        // Set isJoined as true if there's a pending or accepted request
-        return Idea.fromMap(data, doc.id)..isJoined = isPendingOrAccepted;
-      }).toList();
+      // Filter ideas to exclude those owned by the current user
+      return snapshot.docs
+          .where((doc) {
+            final members = List<String>.from(doc['members'] ?? []);
+            return members.isEmpty || 
+                  (members[0] != currentUserId && !members.contains(currentUserId)) && 
+                  members.length < doc["maxMembers"];
+          })
+          .map((doc) {
+            final data = doc.data() as Map<String, dynamic>;
+            return Idea.fromMap(data, doc.id)
+            // Pass the document ID to fromMap
+            ..isJoined = querySnapshot.docs.where((d) {
+              return d['ideaId'] == doc.id;
+            }).isNotEmpty; 
+            // set isJoined value
+          })
+          .toList();
     } catch (e) {
       throw Exception('Failed to load ideas: $e');
     }
@@ -85,21 +85,24 @@ class AnnouncementRepository {
     try {
       await firestore.collection('ideas').doc(idea.id).update(idea.toMap());
     } catch (e) {
+      print('Error updating idea: $e');
       throw Exception('Failed to update idea');
     }
   }
 
-  // Send join request to an idea
-  Future<void> sendJoinRequest(Idea idea, String currentUserId) async {
+
+  // Join to Idea
+  Future<void> sendJoinRequest(Idea idea,String cureentUser) async {
     try {
       await firestore.collection('join_requests').add({
-        "ideaId": idea.id,
-        "userId": currentUserId,
+        "ideaId":idea.id,
+        "userId":cureentUser,
         'status': 'pending',
-        'timestamp': Timestamp.now(),
-      });
+        'timestamp': Timestamp.now(), // Convert timestamp to DateTime.
+        });
     } catch (e) {
-      throw Exception('Failed to send join request: $e');
+      print('Error updating idea: $e');
+      throw Exception('Failed to update idea');
     }
   }
 
@@ -115,8 +118,7 @@ class AnnouncementRepository {
       }
       final snapshot = await query.get();
       return snapshot.docs
-          .map(
-              (doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+          .map((doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
           .toList();
     } catch (e) {
       throw Exception('Failed to load paginated ideas: $e');
@@ -131,8 +133,7 @@ class AnnouncementRepository {
           .where('maxMembers', isEqualTo: maxMembers)
           .get();
       return snapshot.docs
-          .map(
-              (doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+          .map((doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
           .toList();
     } catch (e) {
       throw Exception('Failed to load ideas: $e');
@@ -147,14 +148,13 @@ class AnnouncementRepository {
           .where('skills', arrayContains: skill)
           .get();
       return snapshot.docs
-          .map(
-              (doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+          .map((doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
           .toList();
     } catch (e) {
       throw Exception('Failed to load ideas by skill: $e');
     }
   }
-
+  
   // Add a member to an existing idea
   Future<void> addMemberToIdea(String? ideaId, String memberId) async {
     try {
@@ -194,6 +194,24 @@ class AnnouncementRepository {
     }
   }
 
+
+    // Fetch a specific collaborator by userId
+  Future<Collaborator?> fetchCollaboratorOffers(String userId) async {
+    try {
+      final snapshot = await firestore
+          .collection('collaborators')
+          .where('uid', isEqualTo: userId)
+          .get();
+
+      if (snapshot.docs.isNotEmpty) {
+        return Collaborator.fromMap(snapshot.docs.first.data());
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Failed to load collaborator: $e');
+    }
+  }
+
   // Add a real-time listener for fetching a collaborator
   Stream<Collaborator?> streamCollaborator(String userId) {
     return firestore.collection('collaborators').doc(userId).snapshots().map(
@@ -208,32 +226,32 @@ class AnnouncementRepository {
 
   // Update idea status
   Future<void> updateIdeaStatus(String ideaId, String newStatus) async {
-    try {
-      await firestore.collection('ideas').doc(ideaId).update({
-        'status': newStatus,
-      });
-    } catch (e) {
-      throw Exception('Failed to update idea status');
-    }
+  try {
+    await firestore.collection('ideas').doc(ideaId).update({
+      'status': newStatus,
+    });
+  } catch (e) {
+    print('Error updating idea status: $e');
+    throw Exception('Failed to update idea status');
   }
+}
 
   // Stream ideas based on the current user
   Stream<List<Idea>> streamIdeas(String currentUserId) {
-    return firestore
-        .collection('ideas')
+    return firestore.collection('ideas')
         .where('status', isEqualTo: 'open')
         .snapshots()
         .map((snapshot) {
-      return snapshot.docs.where((doc) {
-        final members = List<String>.from(doc['members'] ?? []);
-        return members.isEmpty ||
-            (members[0] != currentUserId && !members.contains(currentUserId)) &&
+          return snapshot.docs.where((doc) {
+            final members = List<String>.from(doc['members'] ?? []);
+            return members.isEmpty ||
+                (members[0] != currentUserId && !members.contains(currentUserId)) && 
                 members.length < doc["maxMembers"];
-      }).map((doc) {
-        final data = doc.data() as Map<String, dynamic>;
-        return Idea.fromMap(data, doc.id);
-      }).toList();
-    });
+          }).map((doc) {
+            final data = doc.data() as Map<String, dynamic>;
+            return Idea.fromMap(data, doc.id);
+          }).toList();
+        });
   }
 
   // Update project status
@@ -247,9 +265,10 @@ class AnnouncementRepository {
     }
   }
 
-  // Accept a join request and hide the join button for the user
-  Future<void> acceptJoinRequest(Idea idea, String userId) async {
+
+ Future<void> acceptJoinRequest(Idea idea, String userId) async {
     try {
+      // Update the join request status to "accepted" in Firestore
       final requestRef = await firestore
           .collection('join_requests')
           .where('ideaId', isEqualTo: idea.id)
@@ -258,28 +277,36 @@ class AnnouncementRepository {
 
       if (requestRef.docs.isNotEmpty) {
         await requestRef.docs.first.reference.update({'status': 'accepted'});
-
+        // Add the requester as a member of the idea
         await firestore.collection('ideas').doc(idea.id).update({
           'members': FieldValue.arrayUnion([userId])
         });
-
+        // Is Full Code
         final int maxMembers = idea.maxMembers;
         final int currentMembers = idea.members.length;
         final bool isFull = currentMembers == maxMembers;
-
-        if (isFull) {
+        if(isFull){
           await updateProjectStatus(idea.id!, "ongoing");
           idea.status = "ongoing";
+          // final requestRef = await firestore
+          //   .collection('join_requests')
+          //   .where('ideaId', isEqualTo: idea.id).get();
+          //   for (var i = 0; i < requestRef.docs.length; i++) {
+          //     await firestore.collection('join_requests').doc(requestRef.docs[i].id).update({
+          //       'status': "rejected",
+          //     });
+          //   }
         }
+        // To Here
       }
     } catch (e) {
       throw Exception('Failed to accept join request.');
     }
   }
 
-  // Reject join request
   Future<void> rejectJoinRequest(String ideaId, String userId) async {
     try {
+      // Update the join request status to "rejected" in Firestore
       final requestRef = await firestore
           .collection('join_requests')
           .where('ideaId', isEqualTo: ideaId)
--- blade/lib/features/announcement/widgets/announcement_card_widget.dart
+++ blade/lib/features/announcement/widgets/announcement_card_widget.dart
@@ -1,6 +1,7 @@
+import 'package:blade_app/features/announcement/bloc/announcement_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../project_info/screens/project_screen.dart';
 import '../src/announcement_model.dart';
 import '../src/announcement_repository.dart';
 import 'avatar_stack_widget.dart';
@@ -9,141 +10,78 @@ import '../screens/members_screen.dart';
 import '../../../utils/constants/colors.dart';
 
 class AnnouncementCardWidget extends StatefulWidget {
+  final Idea idea;
   final AnnouncementRepository repository;
+  final Function() fetchAll;
 
-  const AnnouncementCardWidget({super.key, required this.repository});
+  const AnnouncementCardWidget({
+    super.key,
+    required this.idea,
+    required this.repository, 
+    required this.fetchAll,
+  });
 
   @override
   _AnnouncementCardWidgetState createState() => _AnnouncementCardWidgetState();
 }
 
 class _AnnouncementCardWidgetState extends State<AnnouncementCardWidget> {
-  final ScrollController _scrollController = ScrollController();
-  List<Idea> _ideas = [];
-  bool _isLoading = false;
-  DocumentSnapshot? _lastDoc; // Store the last document for pagination
-  bool _hasMoreData = true;
+  bool isExpanded = false;
+  bool exceedsMaxLines = false;
   String? currentUserId;
+  bool isJoinPending = false;
 
   @override
   void initState() {
     super.initState();
     currentUserId = FirebaseAuth.instance.currentUser?.uid;
-    _loadMoreData(); // Initial data load
-    _scrollController.addListener(_onScroll); // Add scroll listener
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose(); // Dispose the scroll controller
-    super.dispose();
-  }
-
-  // Load more data with Firestore pagination
-  Future<void> _loadMoreData() async {
-    if (_isLoading || !_hasMoreData) return;
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    try {
-      const int limit = 10; // Number of ideas to load per request
-
-      QuerySnapshot snapshot = (await widget.repository.fetchIdeasWithPagination(
-        limit: limit,
-        lastDoc: _lastDoc, // Use the last document for pagination
-      )) as QuerySnapshot<Object?>;
-
-      List<Idea> newIdeas = snapshot.docs
-          .map((doc) => Idea.fromMap(doc.data() as Map<String, dynamic>, doc.id))
-          .toList();
-
-      // Check if we've reached the end of the data
-      if (newIdeas.length < limit) {
-        _hasMoreData = false; // No more data to load
-      }
-
-      setState(() {
-        _ideas.addAll(newIdeas);
-        if (snapshot.docs.isNotEmpty) {
-          _lastDoc = snapshot.docs.last; // Save the last document for pagination
-        }
-      });
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to load ideas: $e')),
-      );
-    }
-
-    setState(() {
-      _isLoading = false;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkTextOverflow();
     });
   }
 
-  // Detect when the user is near the bottom of the list to load more data
-  void _onScroll() {
-    if (_scrollController.position.pixels >=
-        _scrollController.position.maxScrollExtent - 200) {
-      // Load more data when the user is near the bottom of the scroll
-      _loadMoreData();
-    }
+  void _checkTextOverflow() {
+    final textStyle = TextStyle(
+      color: Theme.of(context).textTheme.bodyLarge?.color ?? TColors.textPrimary,
+      fontSize: MediaQuery.of(context).size.width *
+          0.04 *
+          MediaQuery.of(context).textScaleFactor,
+      fontWeight: FontWeight.w400,
+    );
+
+    final span = TextSpan(
+      text: widget.idea.description,
+      style: textStyle,
+    );
+
+    final tp = TextPainter(
+      text: span,
+      maxLines: 4,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+
+    tp.layout(maxWidth: MediaQuery.of(context).size.width * 0.9);
+    setState(() {
+      exceedsMaxLines = tp.didExceedMaxLines;
+    });
   }
 
-  Future<void> _handleJoinRequest(Idea idea) async {
+  void _handleJoinRequest() async {
+
     setState(() {
-      idea.isJoined = true;
+      isJoinPending = true;
+      widget.idea.isJoined = true;
     });
 
-    await widget.repository.sendJoinRequest(idea, currentUserId!);
+    await widget.repository.sendJoinRequest(widget.idea,currentUserId!);
+    widget.fetchAll();
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text('Join request sent. Awaiting approval.')),
     );
+
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ideas'),
-      ),
-      body: _ideas.isEmpty && !_isLoading
-          ? const Center(child: Text('No ideas available'))
-          : ListView.builder(
-              controller: _scrollController,
-              itemCount: _ideas.length + (_hasMoreData ? 1 : 0),
-              itemBuilder: (context, index) {
-                if (index == _ideas.length) {
-                  // Display a loading indicator at the bottom if more data is being loaded
-                  return const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Center(child: CircularProgressIndicator()),
-                  );
-                }
-
-                final idea = _ideas[index];
-                return IdeaCard(
-                  idea: idea,
-                  repository: widget.repository,
-                  onJoinRequest: () => _handleJoinRequest(idea),
-                );
-              },
-            ),
-    );
-  }
-}
-
-class IdeaCard extends StatelessWidget {
-  final Idea idea;
-  final AnnouncementRepository repository;
-  final VoidCallback onJoinRequest;
-
-  const IdeaCard({
-    super.key,
-    required this.idea,
-    required this.repository,
-    required this.onJoinRequest,
-  });
 
   @override
   Widget build(BuildContext context) {
@@ -152,40 +90,58 @@ class IdeaCard extends StatelessWidget {
     final double textScaleFactor = MediaQuery.of(context).textScaleFactor;
     final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
-    final int maxMembers = idea.maxMembers;
-    final int currentMembers = idea.members.length;
+    
+
+    final int maxMembers = widget.idea.maxMembers;
+    final int currentMembers = widget.idea.members.length;
     final int membersNeeded =
         maxMembers > currentMembers ? maxMembers - currentMembers : 0;
 
-    bool canJoin = FirebaseAuth.instance.currentUser?.uid != null &&
-        !idea.isJoined! &&
-        !idea.members.contains(FirebaseAuth.instance.currentUser!.uid) &&
-        currentMembers < maxMembers;
 
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: screenHeight * 0.02,
-        horizontal: screenWidth * 0.05,
-      ),
-      child: Container(
-        width: screenWidth * 0.9,
-        decoration: BoxDecoration(
-          color: isDarkMode ? TColors.container : TColors.white,
-          borderRadius: BorderRadius.circular(23),
-          border: isDarkMode
-              ? null // No border in dark mode
-              : Border.all(color: TColors.borderPrimary), // Light mode border
+    bool canJoin = currentUserId != null &&
+        !widget.idea.isJoined! &&
+        !widget.idea.members.contains(currentUserId) &&
+        currentMembers < maxMembers &&
+        !isJoinPending ;
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => ProjectScreen(
+              canJoin: canJoin,
+              idea: widget.idea,
+              repository: widget.repository,
+               onJoinRequestSent: null,
+            ),
+          ),
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: screenHeight * 0.02,
+          horizontal: screenWidth * 0.05,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+        child: Container(
+          width: screenWidth * 0.9,
+          decoration: BoxDecoration(
+            color: isDarkMode ? TColors.container : TColors.white,
+            borderRadius: BorderRadius.circular(23),
+            border: isDarkMode
+                        ? null // No border in dark mode
+                        : Border.all(color: TColors.borderPrimary), // Light mode border
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
               Row(
                 children: [
                   Expanded(
                     child: Text(
-                      idea.title,
+                      widget.idea.title,
                       style: TextStyle(
                         color: isDarkMode ? TColors.textWhite : TColors.black,
                         fontSize: screenWidth * 0.055 * textScaleFactor,
@@ -196,14 +152,13 @@ class IdeaCard extends StatelessWidget {
                   SizedBox(width: screenWidth * 0.02),
                   GestureDetector(
                     onTap: () {
-                      // Navigate to MembersScreen on tap
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => MembersScreen(
-                            memberIds: idea.members,
-                            ideaSkills: idea.skills,
-                            repository: repository,
+                            memberIds: widget.idea.members,
+                            ideaSkills: widget.idea.skills,
+                            repository: widget.repository,
                           ),
                         ),
                       );
@@ -214,9 +169,9 @@ class IdeaCard extends StatelessWidget {
                       child: SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: AvatarStackWidget(
-                          userIds: idea.members,
+                          userIds: widget.idea.members,
                           screenWidth: screenWidth,
-                          repository: repository,
+                          repository: widget.repository,
                         ),
                       ),
                     ),
@@ -227,13 +182,30 @@ class IdeaCard extends StatelessWidget {
 
               // Description and "Show more" logic
               Text(
-                idea.description,
+                widget.idea.description,
                 style: TextStyle(
                   color: isDarkMode ? TColors.textWhite : TColors.black,
                 ),
-                maxLines: 4,
-                overflow: TextOverflow.ellipsis,
+                
+                maxLines: isExpanded ? null : 4,
+                overflow: isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
               ),
+
+              if (exceedsMaxLines)
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isExpanded = !isExpanded;
+                    });
+                  },
+                  child: Text(
+                    isExpanded ? "Show less" : "Show more",
+                    style: TextStyle(
+                      color: TColors.info,
+                      fontSize: screenWidth * 0.04 * textScaleFactor,
+                    ),
+                  ),
+                ),
               SizedBox(height: screenHeight * 0.02),
 
               // Dynamically display the number of members needed
@@ -267,35 +239,38 @@ class IdeaCard extends StatelessWidget {
                   child: Wrap(
                     spacing: 8.0,
                     runSpacing: 8.0,
-                    children: idea.skills
+                    children: widget.idea.skills
                         .map((skill) => SkillTagWidget(skills: [skill]))
                         .toList(),
                   ),
                 ),
               ),
               SizedBox(height: screenHeight * 0.005),
-
               // Join button
               if (canJoin)
-                Center(
-                  child: Container(
-                    width: screenWidth * 0.4,
-                    height: screenHeight * 0.05,
-                    decoration: BoxDecoration(
-                      color: TColors.primary, // Reduced opacity for disabled look
-                      borderRadius: BorderRadius.circular(48),
+              Center(
+                child: Container(
+                  width: screenWidth * 0.4,
+                  height: screenHeight * 0.05,
+                  decoration: BoxDecoration(
+                    color: TColors.primary, // Reduced opacity for disabled look
+                    borderRadius: BorderRadius.circular(48),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: _handleJoinRequest,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: isJoinPending
+                          ? Colors.grey // Grey color for waiting status
+                          : TColors.primary,
+                      // Regular color for join button
+                      padding: const EdgeInsets.symmetric(horizontal: 18.0),
                     ),
-                    child: ElevatedButton(
-                      onPressed: onJoinRequest,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: TColors.primary, // Regular color for join button
-                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                      ),
-                      child: const Text('Join'),
-                    ),
+                    child: Text(isJoinPending ? 'Waiting' : 'Join'),
                   ),
                 ),
+              ),
             ],
+            ),
           ),
         ),
       ),
