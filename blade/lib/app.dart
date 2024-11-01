ad63ab7f0068d9b37421ed7ea685381d851fbc15
NajlaaHZ <147070686+NajlaaHZ@users.noreply.github.com>
2024-11-02 01:48:27 +0300

    something also

--- blade/lib/app.dart
+++ blade/lib/app.dart
@@ -5,48 +5,50 @@ import 'features/authentication/bloc/authentication_bloc.dart';
 import 'features/authentication/bloc/authentication_event.dart';
 import 'features/authentication/bloc/authentication_state.dart';
 import 'features/authentication/src/authentication_repository.dart';
+import 'features/investment_request/src/investment_request_repository.dart';
 import 'features/notification/src/NotificationService.dart';
 import 'features/profile/bloc/repository/profile_repository.dart';
-class App extends StatefulWidget {
+
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  final NotificationService notificationService = NotificationService();
-
-  @override
-  void dispose() {
-    // Clean up the notification listener
-    notificationService.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     final authenticationRepository = AuthenticationRepository();
-    final profileRepository = ProfileRepository();
+    final profileRepository = ProfileRepository(); // Add ProfileRepository
+    final investmentRequestRepository = InvestmentRequestRepository();
+    final NotificationService notificationService =
+        NotificationService();
 
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider.value(value: authenticationRepository),
-        RepositoryProvider.value(value: profileRepository),
+        RepositoryProvider.value(
+            value: profileRepository), // Provide ProfileRepository
+        RepositoryProvider.value(value: investmentRequestRepository),
       ],
       child: BlocProvider(
+        create: (context) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+        )..add(AppStarted()),
+         child: BlocProvider(
+        create: (context) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+        )..add(AppStarted()),
+              child: BlocProvider(
         create: (context) => AuthenticationBloc(
           authenticationRepository: authenticationRepository,
         )..add(AppStarted()),
         child: BlocListener<AuthenticationBloc, AuthenticationState>(
           listener: (context, state) {
             if (state is AuthenticationAuthenticated) {
-              // Start listening to notifications for the authenticated user
+              // Use uid instead of id for the unique identifier
               notificationService.listenToFirebaseNotifications(state.user.uid);
             }
           },
           child: const AppView(),
         ),
+              ),)
       ),
     );
   }
