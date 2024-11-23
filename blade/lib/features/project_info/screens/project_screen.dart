1e3e859dc6d41eadc12f27f1952488741644aaf0
Raghad <raghadalotibi939@gmail.com>
2024-11-23 23:45:55 +0300

    Duplication solved

--- blade/lib/features/project_info/screens/project_screen.dart
+++ blade/lib/features/project_info/screens/project_screen.dart
@@ -393,7 +393,8 @@ class _ProjectScreenState extends State<ProjectScreen> {
                   //       );
                   //     },
                   //   ),
-                  if (userType == 'collaborator' && isOwner &&
+                  if (userType == 'collaborator' &&
+                      isOwner &&
                       (idea.status == 'open' || idea.status == 'ongoing'))
                     IconButton(
                       icon: const Icon(Icons.settings),
@@ -474,42 +475,6 @@ class _ProjectScreenState extends State<ProjectScreen> {
                         child: SkillTagWidget(skills: idea.skills),
                       ),
                     ),
-                    if (userType == 'collaborator' &&
-                        ((!isOwner && idea.status == 'open') ||
-                            widget.useInvestButton)) ...[
-                      const SizedBox(height: 16),
-                      Center(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: _buttonBackgroundColor,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 36, vertical: 12),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          onPressed: () {
-                            if ((_isRequestPending && _joinRequestId != null)) {
-                              // Cancel the join request
-                              _cancelJoinRequest(_joinRequestId!);
-                            } else if (_isMember) {
-                              // Leave the project
-                              _leaveProject();
-                            } else {
-                              // Send a join request
-                              _sendJoinRequest();
-                            }
-                          },
-                          child: Text(
-                            _buttonText,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
                     if (userType == 'collaborator' &&
                         ((!isOwner && idea.status == 'open') ||
                             widget.useInvestButton)) ...[
