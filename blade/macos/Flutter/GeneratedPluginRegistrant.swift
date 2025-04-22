bc23b31d73fc29e3508a869222a61bd9750d5920
AljawharahAlotaibi <145213331+AljawharahAlotaibi@users.noreply.github.com>
2025-04-22 22:56:42 +0300

    AFER FACTORING

--- blade/macos/Flutter/GeneratedPluginRegistrant.swift
+++ blade/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_core
 import firebase_storage
 import flutter_local_notifications
 import flutter_secure_storage_macos
-import flutter_web_auth
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -28,7 +27,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
